8b9200c1200b4f05e1982281731c7cb13b94a58a
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2024-02-06 21:14:35 +0000

    Initial

---
+++ calendertime-to-str/popup.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react"
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+
+
+
+function IndexPopup() {
+  const calenderEl = document.createElement("div") as HTMLElement
+const calender = new Calendar(calenderEl,{
+  plugins: [ dayGridPlugin, timeGridPlugin, listPlugin ],
+  initialView: 'dayGridMonth',
+  headerToolbar: {
+    left: 'prev,next today',
+    center: 'title',
+    right: 'dayGridMonth,timeGridWeek,listWeek'
+  }
+})
+  const [data, setData] = useState("")
+  calender.render()
+  return (
+    <div
+      style={{
+        padding: 16
+      }}>
+      <h2>
+        Welcome to your{" "}
+        <a href="https://www.plasmo.com" target="_blank">
+          Plasmo
+        </a>{" "}
+        Extension!
+      </h2>
+      <input onChange={(e) => setData(e.target.value)} value={data} />
+      <a href="https://docs.plasmo.com" target="_blank">
+        View Docs
+      </a>
+    </div>
+  )
+}
+
+export default IndexPopup
